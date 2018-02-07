bbddfd7d3d858067e80d612a4b79f437d30a4347
SkyLined <github@skylined.nl>
2018-02-07 09:06:34 +0100

    "License.py show" can now show details of a single license

--- License_fuMain_Show.py
+++ License_fuMain_Show.py
@@ -2,27 +2,34 @@ import re;
 
 from mProductVersionAndLicense.cProductDetails import cProductDetails;
 from mProductVersionAndLicense.cLicenseCheckRegistry import cLicenseCheckRegistry;
+from mProductVersionAndLicense.cLicenseCollection import cLicenseCollection;
 
 def fUsage(sMainScriptName, sFeatureName):
          ################################################################################
-  print "Show all licenses registered for use by the current user on this system.";
+  print "Show all licenses registered for use by the current user on this system. Or in";
+  print "a particular file.";
   print "Usage:";
   print "  %s %s <options>" % (sMainScriptName, sFeatureName);
   print "Where <options> is:";
   print " [[--product=]<product folder>]    The root folder for the product.";
+  print " [[--license=]<license file name>] Path to a file containing the license(s).";
   print "";
-  print "This feature will show all licenses in the registry, or only those for a";
-  print "specific product. When a specific product is provided, the licenses will";
-  print "be checked for validity with the server";
+  print "This feature will show all licenses in the registry, if no arguments are";
+  print "provided, or only those in the registry for a specific product, or all licenses";
+  print "in a specific license file. When a specific product is provided, the licenses";
+  print "will be checked for validity with the server";
 
 def License_fuMain_Show(sMainScriptName, sFeatureName, asArguments):
   # Parse arguments
   sProductFolderPath = None;
+  sLicenseFilePath = None;
   for sArgument in asArguments:
     oArgumentMatch = re.match(r"^\-\-([\w\-]+)(?:\=(.*))?$", sArgument);
     if not oArgumentMatch:
       if sProductFolderPath is None:
         sProductFolderPath = sArgument;
+      elif sLicenseFilePath is None:
+        sLicenseFilePath = sArgument;
       else:
         print "- Superfluous argument %s!" % sArgument;
         return 1;
@@ -37,6 +44,11 @@ def License_fuMain_Show(sMainScriptName, sFeatureName, asArguments):
           print "- Please provide only one product folder path!";
           return 1;
         sProductFolderPath = sValue;
+      elif sName == "license":
+        if sLicenseFilePath is not None:
+          print "- Please provide only one input file path!";
+          return 1;
+        sLicenseFilePath = sValue;
       else:
         print "- Unrecognized argument %s!" % sArgument;
         return 1;
@@ -50,12 +62,24 @@ def License_fuMain_Show(sMainScriptName, sFeatureName, asArguments):
       print "- Product details could not be read from %s!" % sProductFolderPath;
       return 2;
   
-  oLicenseCollection = cLicenseCheckRegistry.foLicenseCollectionFromRegistry(
-      sProductName = oProductDetails and oProductDetails.sProductName);
-  # Read the licenses from the input file for the given product
-  if len(oLicenseCollection.aoLicenses) == 0:
-    print "- There are no licenses registered for the current user on this system!";
-    return 2;
+  if sLicenseFilePath is None:
+    # Read the licenses from the registry (for the given product is provided)
+    oLicenseCollection = cLicenseCheckRegistry.foLicenseCollectionFromRegistry(
+        sProductName = oProductDetails and oProductDetails.sProductName);
+    if len(oLicenseCollection.aoLicenses) == 0:
+      print "- There are no licenses in the registry%s for the current user on this system!" % \
+          (oProductDetails and " for product %s" % oProductDetails.sProductName or "");
+      return 2;
+  else:
+    # Read the licenses from the input file (for the given product is provided)
+    oLicenseCollection = cLicenseCollection.foReadFromFile(
+      sLicenseFilePath = sLicenseFilePath,
+      sProductName = oProductDetails and oProductDetails.sProductName,
+    );
+    if len(oLicenseCollection.aoLicenses) == 0:
+      print "- There are no licenses in the file%s!"  % \
+          (oProductDetails and " for product %s" % oProductDetails.sProductName or "");
+      return 2;
   # Check the licenses with the server:
   if oProductDetails:
     oLicenseCollection.fCheckWithServer(oProductDetails.oLicenseCheckServer, bWriteToRegistry = False); # Do not write to registry yet
