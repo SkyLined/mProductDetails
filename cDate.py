b223c32079244257cb18140a8518f4d47a4d5b0f
SkyLined <github@skylined.nl>
2018-04-10 09:32:19 +0200

    Update cDate to latest

--- cDate.py
+++ cDate.py
@@ -2,7 +2,36 @@ import datetime, re;
 
 ### cDate ###
 grDate = re.compile(r"^(\d{4})[\-\/](\d{1,2})[\-\/](\d{1,2})$");
+grDurationComponent = re.compile(r"^(\d+)([ymd])$");
+gasMonths = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+gasOrdinalPostfixes = [
+  "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"
+];
 class cDate(datetime.date):
+  @property
+  def uYear(oSelf):
+    return oSelf.year;
+  @uYear.setter
+  def uYear(oSelf, uYear):
+    oSelf.year = uYear;
+  
+  @property
+  def uMonth(oSelf):
+    return oSelf.month;
+  @uMonth.setter
+  def uMonth(oSelf, uMonth):
+    oSelf.month = uMonth;
+  
+  @property
+  def uDay(oSelf):
+    return oSelf.day;
+  @uDay.setter
+  def uDay(oSelf, uDay):
+    oSelf.day = uDay;
+  
   @staticmethod
   def foNow():
     oNow = datetime.datetime.utcnow();
@@ -13,19 +42,19 @@ class cDate(datetime.date):
     return oDateMatch and cDate(*[long(s) for s in oDateMatch.groups()]);
   
   def foEndDateForDuration(oSelf, sDuration):
-    oEndDate = cDate(oSelf.year, oSelf.month, oSelf.day);
+    oEndDate = cDate(oSelf.uYear, oSelf.uMonth, oSelf.uDay);
     for sDurationComponent in sDuration.split("+"):
-      oDurationComponentMatch = re.match(r"(\d+)([ymd])", sDurationComponent.lower());
+      oDurationComponentMatch = re.match(grDurationComponent, sDurationComponent.lower());
       assert oDurationComponentMatch, \
           "Invalid duration %s" % sDuration;
       (sCount, sType) = oDurationComponentMatch.groups();
       uCount = long(sCount);
       if sType == "y":
-        oEndDate = oEndDate.replace(year = oEndDate.year + uCount);
+        oEndDate = oEndDate.replace(year = oEndDate.uYear + uCount);
       elif sType == "m":
-        uNewMonth = oEndDate.month + uCount;
+        uNewMonth = oEndDate.uMonth + uCount;
         while uNewMonth > 12:
-          oEndDate = oEndDate.replace(year = oEndDate.year + 1);
+          oEndDate = oEndDate.replace(year = oEndDate.uYear + 1);
           uNewMonth -= 12;
         oEndDate = oEndDate.replace(month = uNewMonth);
       elif sType == "d":
@@ -33,6 +62,14 @@ class cDate(datetime.date):
       else:
         raise AssertionError("NOT REACHED");  
     return oEndDate;
+  
+  def fsToHumanReadbleString(oSelf):
+    # Month <day>th, <year>
+    return "%s %d%s, %d" % (
+      gasMonths[oSelf.uMonth],
+      oSelf.uDay, gasOrdinalPostfixes[oSelf.uDay % 10],
+      oSelf.uYear,
+    );
   
   def __str__(oSelf):
     return oSelf.isoformat();
