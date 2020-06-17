70e9647843b6200c85d867bcd58f22786be18d3b
SkyLined <github@skylined.nl>
2020-06-17 11:36:51 +0200

    Improve tests

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,50 +1,52 @@
-import os, sys;
+from fTestDependencies import fTestDependencies;
+fTestDependencies();
 
-# Augment the search path: pretend we are in the parent folder so we can load mProductDetails as a module:
-sTestsFolderPath = os.path.abspath(os.path.dirname(__file__));
-sMainFolderPath = os.path.abspath(os.path.dirname(sTestsFolderPath));
-sParentFolderPath = os.path.normpath(os.path.join(sMainFolderPath, ".."));
-sys.path = [sParentFolderPath] + sys.path;
+from mDebugOutput import fEnableDebugOutputForClass, fEnableDebugOutputForModule, fTerminateWithException;
+try:
+  import os, sys;
 
-import mProductDetails;
-from mProductDetails.fsToOxfordComma import fsToOxfordComma;
+  import mProductDetails;
+  from mProductDetails.fsToOxfordComma import fsToOxfordComma;
 
-print "Unique system id: " + mProductDetails.fsGetSystemId();
+  print "Unique system id: " + mProductDetails.fsGetSystemId();
 
-print "Product version information:";
-oMainProductDetails = mProductDetails.foGetProductDetailsForModule(mProductDetails);
-aoProductDetails = mProductDetails.faoGetProductDetailsForAllLoadedModules();
-# Change order to put main product at the front:
-aoProductDetails.remove(oMainProductDetails);
-aoProductDetails.insert(0, oMainProductDetails);
-for oProductDetails in aoProductDetails:
-  print "+ \"%s\" version \"%s\" by \"%s\" installed in \"%s\"." % \
-      (oProductDetails.sProductName, oProductDetails.oProductVersion, oProductDetails.sProductAuthor, oProductDetails.sInstallationFolderPath);
-print;
-
-print "Checking licenses for loaded software products:";
-oLicenseCollection = mProductDetails.foGetLicenseCollectionForAllLoadedProducts();
-(asErrors, asWarnings) = oLicenseCollection.ftasGetLicenseErrorsAndWarnings();
-if asErrors:
-  print "Software license error%s:" % (len(asErrors) > 1 and "s" or "");
-  for sError in asErrors:
-    print "- " + sError;
+  print "Product version information:";
+  oMainProductDetails = mProductDetails.foGetProductDetailsForModule(mProductDetails);
+  aoProductDetails = mProductDetails.faoGetProductDetailsForAllLoadedModules();
+  # Change order to put main product at the front:
+  aoProductDetails.remove(oMainProductDetails);
+  aoProductDetails.insert(0, oMainProductDetails);
+  for oProductDetails in aoProductDetails:
+    print "+ \"%s\" version \"%s\" by \"%s\" installed in \"%s\"." % \
+        (oProductDetails.sProductName, oProductDetails.oProductVersion, oProductDetails.sProductAuthor, oProductDetails.sInstallationFolderPath);
   print;
 
-if asWarnings:
-  print "Software license warning%s:" % (len(asWarnings) > 1 and "s" or "");
-  for sWarning in asWarnings:
-    print "* " + sWarning;
-  print;
+  print "Checking licenses for loaded software products:";
+  oLicenseCollection = mProductDetails.foGetLicenseCollectionForAllLoadedProducts();
+  (asErrors, asWarnings) = oLicenseCollection.ftasGetLicenseErrorsAndWarnings();
+  if asErrors:
+    print "Software license error%s:" % (len(asErrors) > 1 and "s" or "");
+    for sError in asErrors:
+      print "- " + sError;
+    print;
 
-print "Software license information in registry:";
-aoLicenses = mProductDetails.faoGetLicensesFromRegistry();
-if not aoLicenses:
-  print "- No licenses loaded";
-for oLicense in aoLicenses:
-  print "+ %s licensed to %s:" % (oLicense.asProductNames[0], oLicense.sLicenseeName);
-  print "  Products   : %s" % fsToOxfordComma(oLicense.asProductNames);
-  print "  Usage type : %s" % oLicense.sUsageTypeDescription;
-  print "  Instances  : %s" % oLicense.uLicensedInstances;
-  print "  End date   : %s" % oLicense.oEndDate.fsToHumanReadableString();
-  print "  Id         : %s" % oLicense.sLicenseId;
+  if asWarnings:
+    print "Software license warning%s:" % (len(asWarnings) > 1 and "s" or "");
+    for sWarning in asWarnings:
+      print "* " + sWarning;
+    print;
+
+  print "Software license information in registry:";
+  aoLicenses = mProductDetails.faoGetLicensesFromRegistry();
+  if not aoLicenses:
+    print "- No licenses loaded";
+  for oLicense in aoLicenses:
+    print "+ %s licensed to %s:" % (oLicense.asProductNames[0], oLicense.sLicenseeName);
+    print "  Products   : %s" % fsToOxfordComma(oLicense.asProductNames);
+    print "  Usage type : %s" % oLicense.sUsageTypeDescription;
+    print "  Instances  : %s" % oLicense.uLicensedInstances;
+    print "  End date   : %s" % oLicense.oEndDate.fsToHumanReadableString();
+    print "  Id         : %s" % oLicense.sLicenseId;
+
+except Exception as oException:
+  fTerminateWithException(oException);
