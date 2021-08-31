6faeb5abdbf90383e0bee6971e05af0a6ae23a9c
SkyLined <github@skylined.nl>
2021-08-31 22:18:45 +0200

    `faoGetProductDetailsForAllLoadedModules` now caches globally

--- faoGetProductDetailsForAllLoadedModules.py
+++ faoGetProductDetailsForAllLoadedModules.py
@@ -1,18 +1,18 @@
 import sys;
 
+gdoProductDetails_by_sProductName = {};
 def faoGetProductDetailsForAllLoadedModules():
-  doProductDetails_by_sProductName = {};
   for mLoadedModule in sys.modules.values():
     o0ProductDetails = cProductDetails.fo0GetForModule(mLoadedModule);
     if o0ProductDetails is None: continue;
     oProductDetails = o0ProductDetails;
-    oExistingProductDetails = doProductDetails_by_sProductName.get(oProductDetails.sProductName);
+    oExistingProductDetails = gdoProductDetails_by_sProductName.get(oProductDetails.sProductName);
     if oExistingProductDetails:
       assert oProductDetails.s0InstallationFolderPath == oExistingProductDetails.s0InstallationFolderPath, \
           "%s is loaded from both %s and %s!?" % \
           (oProductDetails.s0InstallationFolderPath, oExistingProductDetails.s0InstallationFolderPath);
     else:
-      doProductDetails_by_sProductName[oProductDetails.sProductName] = oProductDetails;
-  return list(doProductDetails_by_sProductName.values());
+      gdoProductDetails_by_sProductName[oProductDetails.sProductName] = oProductDetails;
+  return list(gdoProductDetails_by_sProductName.values());
 
 from .cProductDetails import cProductDetails;
